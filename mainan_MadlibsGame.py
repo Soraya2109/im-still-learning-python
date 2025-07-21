aa99f2f787d91a2a778b4f3da6497012648bd35b
soraawistaria <amaliasoraya2109@gmail.com>
2025-07-21 10:48:48 +0800

    Add files via upload

---
+++ mainan_MadlibsGame.py
@@ -0,0 +1,15 @@
+# Madlibs game
+
+name = input("fill ur name : ")
+gender = input("F/L : ")
+sul = input("someone ur love : ")
+relation = input("ur crush's gf/bf : ")
+
+
+
+print(f"One day, on a town exist {name}")
+print(f"{gender} fall for {sul}")
+print(f"but... {gender} get rejected, because {sul} already has a {relation}")
+print(f"{gender} cry a lot, appetite, then get sick")
+print(f"{gender} wish that someone {gender} come and see {gender}")
+print(f"unfortunately {sul} didn't come.")
